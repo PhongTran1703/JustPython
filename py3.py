87b8e7a1ea07b5ec72dbffe9ec64d321c37a697a
Phong Tran <thanhphongtran1703@gmail.com>
2025-10-30 14:13:18 +0000

    basic py

---
+++ py3.py
@@ -0,0 +1,7 @@
+name = "Bro code"
+age = 22
+gpa = 3.2
+is_student = True
+
+gpa = int(gpa)
+print(type(gpa))
---
+++ py4.py
@@ -0,0 +1,5 @@
+
+length = input("Enter the length: ")
+width = input("Enter the width: ")
+area = float(length) * float(width)
+print(f"The area is: {area} cm²")
